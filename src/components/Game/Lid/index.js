4e8d45a2d3f33049fedb96c62eaa242d81f172be
drivenfw <ivan.petrochenko@gmail.com>
2018-06-20 23:06:52 +0300

    Lid animations reimplemented with react-transition-group

--- src/components/Game/Lid/index.js
+++ src/components/Game/Lid/index.js
@@ -2,6 +2,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import classNames from 'classnames'
+import { Transition } from 'react-transition-group'
 
 // CSS
 import './styles.sass'
@@ -12,12 +13,20 @@ const mapStateToProps = ({ layout: { lid } }) => ({
 })
 
 const Lid = ({ lid }) => (
-  <React.Fragment>
-    <div className={classNames('lid back', { open: !lid })} />
-    <div className={classNames('lid front', { open: !lid })}>
-      <label>15 Puzzle</label>
-    </div>
-  </React.Fragment>
+  <Transition
+    in={lid}
+    timeout={2000}
+    unmountOnExit
+  >
+    {state => (
+      <React.Fragment>
+        <div className={classNames('lid back', state)} />
+        <div className={classNames('lid front', state)}>
+          <label>15 Puzzle</label>
+        </div>
+      </React.Fragment>
+    )}
+  </Transition>
 )
 
 export default connect(mapStateToProps)(Lid)
