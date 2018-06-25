d4c1b75aac61b5e8241337ac855ea14c2b98ed04
drivenfw <ivan.petrochenko@gmail.com>
2018-06-25 13:41:09 +0300

    Congrats are shown with animations

--- src/components/Game/Congrats/index.js
+++ src/components/Game/Congrats/index.js
@@ -1,6 +1,8 @@
 // Libs
 import React from 'react'
 import { connect } from 'react-redux'
+import classNames from 'classnames'
+import { Transition } from 'react-transition-group'
 
 // CSS
 import './styles.sass'
@@ -11,10 +13,20 @@ const mapStateToProps = ({ layout: { solved } }) => ({
 })
 
 const Congrats = ({ show }) => (
-  show &&
-    <div className="congrats">
-      <label>Congrats!!!<br />You solved the puzzle!</label>
-    </div>
+  <Transition
+    in={show}
+    timeout={{
+      enter: 100,
+      exit: 800
+    }}
+    unmountOnExit
+  >
+    {state => 
+      <div className={classNames('congrats', state)}>
+        <label>Congrats!!!<br /> You solved the<br /> puzzle!</label>
+      </div>
+    }
+  </Transition>
 )
 
 export default connect(mapStateToProps)(Congrats)
