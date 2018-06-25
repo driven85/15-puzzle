d5e76248e2e596b9fe507d8854f990d9afc66491
drivenfw <ivan.petrochenko@gmail.com>
2018-06-25 16:00:26 +0300

    LED font added

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,17 @@ module.exports = {
     }, {
       test: /\.(c|sa|sc)ss$/,
       use: ['style-loader', 'css-loader', 'sass-loader'] 
+    },
+    {
+      test: /\.(woff|woff2|eot|ttf|otf)$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            outputPath: 'fonts/'
+          }
+        }
+      ]
     }]
   }
 }
