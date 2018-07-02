e5f96440a0ecffd87cb1c435cf4567e4c5288837
drivenfw <ivan.petrochenko@gmail.com>
2018-07-02 13:04:48 +0300

    The Dialog component opens and closes with animation

--- src/components/UI/Dialog/index.js
+++ src/components/UI/Dialog/index.js
@@ -1,21 +1,43 @@
 // Libs
 import React from 'react'
+import { Transition } from 'react-transition-group'
+import classNames from 'classnames'
 
 // CSS
 import './styles.sass'
 
 
-const Dialog = ({ width, height, children }) => (
-  <div className="dialog">
-    <div className="backdrop">
-      <div 
-        className="content"
-        style={{ width, height }}
-      >
-        {children} 
+const Dialog = ({ 
+  show, 
+  width, 
+  height, 
+  children,
+  onClose
+}) => (
+  <Transition
+    in={show}
+    timeout={{
+      enter: 0,
+      exit: 900
+    }}
+    unmountOnExit
+  >
+    {state => (
+      <div className={classNames('dialog', state)}>
+        <div 
+          className="backdrop"
+          onClick={onClose}
+        >
+          <div 
+            className="content"
+            style={{ width, height }}
+          >
+            {children} 
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
+    )}
+  </Transition>
 )
 
 export default Dialog
