f126836d415920efc1979dc56ca619fafc357481
drivenfw <ivan.petrochenko@gmail.com>
2018-07-04 22:11:18 +0300

    Bug fixed where clicking on the dialog itself would close it

--- src/components/UI/Dialog/index.js
+++ src/components/UI/Dialog/index.js
@@ -1,5 +1,5 @@
 // Libs
-import React from 'react'
+import React, { Component } from 'react'
 import { Transition } from 'react-transition-group'
 import classNames from 'classnames'
 
@@ -7,38 +7,40 @@ import classNames from 'classnames'
 import './styles.sass'
 
 
-const Dialog = ({ 
-  show, 
-  width, 
-  height, 
-  children,
-  onClose
-}) => (
-  <Transition
-    in={show}
-    timeout={{
-      enter: 0,
-      exit: 900
-    }}
-    unmountOnExit
-  >
-    {state => (
-      <div className={classNames('dialog', state)}>
-        <div 
-          className="backdrop"
-          onClick={onClose}
-        >
-          <div 
-            className="content"
-            style={{ width, height }}
-          >
-            {children} 
-          </div>
-        </div>
-      </div>
-    )}
-  </Transition>
-)
+export default class Dialog extends Component {
+  handleBackdropClick = e => {
+    const { onClose } = this.props
 
-export default Dialog
+    if (e.target === e.currentTarget)
+      onClose()
+  }
+
+  render() {
+    const { show, width, height, children } = this.props
+
+    return (
+      <Transition
+        in={show}
+        timeout={{ enter: 0, exit: 900 }}
+        unmountOnExit
+      >
+        {state => (
+          <div className={classNames('dialog', state)}>
+            <div 
+              className="backdrop"
+              onClick={this.handleBackdropClick}
+            >
+              <div 
+                className="content"
+                style={{ width, height }}
+              >
+                {children} 
+              </div>
+            </div>
+          </div>
+        )}
+      </Transition>
+    )
+  }
+}
 
