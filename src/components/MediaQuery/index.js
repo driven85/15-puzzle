7d3bae8d1fa860b816c5fd2bbd5bfb4115ba9a66
drivenfw <ivan.petrochenko@gmail.com>
2018-07-04 21:11:59 +0300

    The MediaQuery component implemented

--- src/components/MediaQuery/index.js
+++ src/components/MediaQuery/index.js
@@ -1,23 +1,47 @@
 // Libs
-import React from 'react'
+import React, { Component } from 'react'
 
 // Context
 import { MediaQueryContext } from './context.js'
 
 
 class MediaQuery extends Component {
+  state = { layout: 'xs' }
+
+  checkMediaQuery = () => {
+    let layout = 'xs'
+
+    for (const bp of Object.keys(MediaQuery.breakpoints)) {
+      if (window.matchMedia(`(min-width: ${bp}px)`).matches) {
+        layout = MediaQuery.breakpoints[bp]
+      }  
+    }
+
+    this.setState({ layout })
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.checkMediaQuery)
+    this.checkMediaQuery()
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.checkMediaQuery)
+  }
+
   render() {
     const { children } = this.props
+    const { layout } = this.state
 
     return (
-      <div className="media-query">
-        <MediaQueryContext.Provider>
-          {children}
-        </MediaQueryContext.Provider>
-      </div>
+      <MediaQueryContext.Provider value={{ layout }}>
+        {children}
+      </MediaQueryContext.Provider>
     )
   }
 }
+
+MediaQuery.breakpoints = { 520: 'sm', 768: 'md', 1024: 'lg' }
 
 export default MediaQuery
 
