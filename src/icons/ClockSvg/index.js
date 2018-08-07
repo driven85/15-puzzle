08e6b9dbe90b5b20e1e540df6ca88d408daf79dd
drivenfw <ivan.petrochenko@gmail.com>
2018-08-07 22:33:25 +0300

    Refactoring

--- src/icons/ClockSvg/index.js
+++ src/icons/ClockSvg/index.js
@@ -1,5 +1,6 @@
 // Libs
 import React from 'react'
+import classNames from 'classnames'
 
 // CSS
 import './styles.sass'
@@ -9,7 +10,7 @@ const ClockSvg = ({ className }) => (
   <svg 
     xmlns="http://www.w3.org/2000/svg" 
     viewBox="0 0 24 24"
-    className={`clock-svg ${className}`}
+    className={classNames('clock-svg', className)}
   >
     <path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"/>
     <path d="M0 0h24v24H0z" fill="none"/>
