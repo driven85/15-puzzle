cb41b1609d6972255b733d8b424e7b74f5927741
drivenfw <ivan.petrochenko@gmail.com>
2018-08-11 00:12:50 +0300

    The getStyle helper can extract all styles for a certain breakpoint as well as styles for a particular element

--- src/helpers/styles.js
+++ src/helpers/styles.js
@@ -1,10 +1,27 @@
+export const deepMerge = (a, b) => {
+  const result = { ...a }
+
+  Object.entries(b).forEach(([key, value]) => {
+    result[key] = value instanceof Object
+      ? deepMerge(a[key], value)
+      : value
+  })
+
+  return result
+}
+
 export const getStyle = (styles, layout, element) => {
   const breakpoints = ['xs', 'sm', 'md', 'lg']
   let style = {}
 
   for (const bp of breakpoints) {
-    const bpStyle = styles[bp] && styles[bp][element] || style // TODO: .?
-    style = { ...style, ...bpStyle }
+    if (element) {
+      const bpStyle = styles[bp] && styles[bp][element] || style // TODO: .?
+      style = { ...style, ...bpStyle }
+    } else {
+      if (styles[bp])
+        style = deepMerge(style, styles[bp])
+    }
 
     if (bp === layout) break
   }
--- src/helpers/styles.test.js
+++ src/helpers/styles.test.js
@@ -1,14 +1,44 @@
 import test from 'tape'
-import { getStyle } from './styles'
+import { deepMerge, getStyle } from './styles'
 
+
+test('deepMerge', t => {
+  let a, b
+
+  a = { a: 1, b: { c: 2, d: 3 }, e: 5 }
+  b = { b: { c: 1 }, d: 4 }
+  t.deepEqual(
+    deepMerge(a, b),
+    { a: 1, b: { c: 1, d: 3 }, d: 4, e: 5 },
+    'Should merge correctly'
+  )
+
+  a = { a: 'hello', b: { c: 'yep', d: { e: true, f: false } } }
+  b = { b: { d: { f: null, g: [1, 2, 3] } } }
+  t.deepEqual(
+    deepMerge(a, b),
+    { a: 'hello', b: { c: 'yep', d: { e: true, f: null, g: [1, 2, 3] } } },
+    'Should merge correctly'
+  )
+
+  a = { a: { b: { c: 1, d: '2', e: ['yep', 5, true, false] } } }
+  b = { a: { b: { e: [1, 2, 3] } } }
+  t.deepEqual(
+    deepMerge(a, b),
+    { a: { b: { c: 1, d: '2', e: [1, 2, 3, false] } } },
+    'Should merge correctly'
+  )
+
+  t.end()
+})
 
 const xsHeaderStyle = { fontSize: 16, padding: 10 }
 const xsMainStyle = { fontSize: 16, margin: 10 }
-const xsFooterStyle = { fontSize: 16 }
+const xsFooterStyle = { fontSize: 16, margin: 15 }
 const smHeaderStyle = { fontSize: 20 }
-const mdHeaderStyle = { fontSize: 32, padding: 20 }
+const mdHeaderStyle = { fontSize: 32, padding: 20, margin: 15 }
 const mdMainStyle = { fontSize: 20 }
-const lgFooterStyle = { fontSize: 20 }
+const lgFooterStyle = { fontSize: 20, padding: 10 }
 
 const styles = {
   'xs': {
@@ -34,7 +64,7 @@ const missingStyles = {
   }
 }
 
-test('getStyle', t => {
+test('getStyle for element', t => {
   t.equal(
     getStyle(styles, 'xs', 'header').fontSize, 
     xsHeaderStyle.fontSize,
@@ -145,6 +175,50 @@ test('getStyle', t => {
     getStyle(missingStyles, 'md', 'header').fontSize,
     xsHeaderStyle.fontSize,
     'Should work correctly if styles for some breakpoints are missing'
+  )
+
+  t.end()
+})
+
+test('getStyle for layout', t => {
+  t.deepEqual(
+    getStyle(styles, 'xs'),
+    {
+      header: xsHeaderStyle,
+      main: xsMainStyle,
+      footer: xsFooterStyle
+    },
+    'Should retrieve xs styles correctly'
+  )
+
+  t.deepEqual(
+    getStyle(styles, 'sm'),
+    {
+      header: { fontSize: 20, padding: 10 },
+      main: xsMainStyle,
+      footer: xsFooterStyle
+    },
+    'Should retrieve sm styles correctly'
+  )
+
+  t.deepEqual(
+    getStyle(styles, 'md'),
+    {
+      header: mdHeaderStyle,
+      main: { fontSize: 20, margin: 10 },
+      footer: xsFooterStyle
+    },
+    'Should retrieve md styles correctly'
+  )
+
+  t.deepEqual(
+    getStyle(styles, 'lg'),
+    {
+      header: mdHeaderStyle,
+      main: { fontSize: 20, margin: 10 },
+      footer: { fontSize: 20, margin: 15, padding: 10 }
+    },
+    'Should retrieve lg styles correctly'
   )
 
   t.end()
