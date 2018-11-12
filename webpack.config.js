3933369e5f9ecb0df17a1e6939ce1a2e7644c40d
drivenfw <ivan.petrochenko@gmail.com>
2018-11-12 21:54:21 +0300

    Bug fixed

--- webpack.config.js
+++ webpack.config.js
@@ -7,8 +7,7 @@ module.exports = () => ({
 
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js',
-    publicPath: 'build/'
+    filename: 'bundle.js'
   },
 
   resolve: {
